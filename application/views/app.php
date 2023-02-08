ec7a9bc54d286806d7f34322aa8b97fbe3bf121f
ryumada <rizukiryumada@gmail.com>
2023-02-08 17:14:11 +0700

    added benchmarking for memory consumption ...to footer

--- application/views/app.php
+++ application/views/app.php
@@ -56,6 +56,9 @@
 	<footer class="footer mt-auto py-1">
 		<div class="container-fluid px-2 text-center">
 			<span class="text-muted">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></span>
+			<?php if(ENVIRONMENT === 'development'): ?>
+				<p class="text-muted">Memory Consumption: {memory_usage}</p>
+			<?php endif; ?>
 		</div>
 	</footer>
 	<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js');?>""></script>
