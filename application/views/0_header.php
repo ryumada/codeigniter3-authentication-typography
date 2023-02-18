03ecc9d6946056d40d78496904eb0cdcc0a63249
ryumada <rizukiryumada@gmail.com>
2023-02-18 11:35:51 +0700

    reorganize dependencies

--- application/views/0_header.php
+++ application/views/0_header.php
@@ -9,5 +9,5 @@
 	<title><?= $titlepage ?></title>
 
 	<link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
